e32f97dc82aadd60b4340e8c15f77c9e044c09bf
Tatum Mortimer <tatumdmortimer@gmail.com>
2014-12-15 13:03:30 -0600

    Changed script to check headers at the top of the file for correct columns to use

--- enaFileParser.py
+++ enaFileParser.py
@@ -16,18 +16,44 @@ inFile = open(sys.argv[1], 'r')
 downloadFile = open(os.path.splitext(sys.argv[1])[0] + '_download.txt', 'w')
 pipelineFile = open(os.path.splitext(sys.argv[1])[0] + '_pipelineIn.txt', 'w')
 
+rgIndex = 100
+sampIndex = 100
+exIndex = 100
+platIndex = 100
+libIndex = 100
+urlIndex = 100
+
 for i, line in enumerate(inFile):
-    if i > 0:
+    if i == 0:
         line = line.strip()
         entries = line.split('\t')
-        readGroup = entries[5]
-        sampleID = entries[3]
-        experimentID = entries[4]
-        platform = entries[7]
+        for i,entry in enumerate(entries):
+            if 'sample_accession' in entry:
+                sampIndex = i
+            elif 'experiment_accession' in entry:
+                exIndex = i
+            elif 'run_accession' in entry:
+                rgIndex = i
+            elif 'instrument' in entry:
+                platIndex = i
+            elif 'library_layout' in entry:
+                libIndex = i
+            elif 'fastq_ftp' in entry:
+                urlIndex = i
+        if 100 in [sampIndex, exIndex, rgIndex, platIndex, libIndex, urlIndex]:
+            print "Cannot find all headers needed"
+            sys.exit(2)
+    else:
+        line = line.strip()
+        entries = line.split('\t')
+        readGroup = entries[rgIndex]
+        sampleID = entries[sampIndex]
+        experimentID = entries[exIndex]
+        platform = entries[platIndex]
         if 'illumina' in platform.lower():
             platform = 'illumina'
-        library = entries[8].lower()
-        readURL = entries[9]
+        library = entries[libIndex].lower()
+        readURL = entries[urlIndex]
         pipelineFile.write('%s\t%s\t%s\t%s\t%s\n' % (readGroup, sampleID, 
                             experimentID, platform, library))
         for url in readURL.split(';'):
