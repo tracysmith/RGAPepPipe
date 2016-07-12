787dc5f345ad7b8711091c6b8d0b8df2fe845d3e
leahrose <lralexander2@wisc.edu>
2016-07-12 14:57:26 -0500

    Changes to realign.sh

--- pooled/realign.sh
+++ pooled/realign.sh
@@ -2,6 +2,6 @@
 
 java -Xmx2g -jar /opt/PepPrograms/RGAPipeline/GenomeAnalysisTK.jar -T RealignerTargetCreator -R /opt/data/mtuberculosis/MtbNCBIH37Rv.fa -I ERR867518.realn.bam -o ERR867518.intervals
 
-java -Xmx4g -jar /opt/PepPrograms/RGAPipeline/GenomeAnalysisTK.jar -T IndelRealigner -R /opt/data/mtuberculosis/MtbNCBIH37Rv.fa -I ERR867518.realn.bam -targetIntervals ERR867518.intervals -nWayOut ERR867518.patrealn.bam
+java -Xmx4g -jar /opt/PepPrograms/RGAPipeline/GenomeAnalysisTK.jar -T IndelRealigner -R /opt/data/mtuberculosis/MtbNCBIH37Rv.fa -I ERR867518.realn.bam -targetIntervals ERR867518.intervals -nWayOut .patrealn.bam
 
 tar -zcvf ERR867518.realign.tar.gz realign/
