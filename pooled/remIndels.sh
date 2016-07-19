36ef2fe2729ac7f177d4249f4cdc201542bcb3fa
ONeillMB1 <ONeillMB1@gmail.com>
2016-07-19 09:57:16 -0500

    changed from pileup to mpileup

--- pooled/remIndels.sh
+++ pooled/remIndels.sh
@@ -2,4 +2,4 @@
 
 perl /opt/PepPrograms/popoolation_1.2.2/basic-pipeline/identify-genomic-indel-regions.pl --input $1.mpileup --output $1.indelreg.gtf
 
-perl /opt/PepPrograms/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input $1.mpileup --gtf $1.indelreg.gtf --output $1.noIndel.pileup
+perl /opt/PepPrograms/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input $1.mpileup --gtf $1.indelreg.gtf --output $1.noIndel.mpileup
