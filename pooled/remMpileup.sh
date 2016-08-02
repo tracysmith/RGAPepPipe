c88d0352dac180162740e7d67840fab757a11336
ONeillMB1 <ONeillMB1@gmail.com>
2016-08-02 11:25:19 -0500

    attempt to solve pat indel removal process

--- pooled/remMpileup.sh
+++ pooled/remMpileup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-perl /opt/PepPrograms/popoolation2_1201/indel_filtering/identify-indel-regions.pl --input $1_Q20_q20.mpileup --output $1.indelreg.gtf
+cat "${@:2}" > $1.indelreg.gtf
 
 perl /opt/PepPrograms/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input $1_Q20_q20.mpileup --gtf $1.indelreg.gtf --output $1.noIndel.mpileup
